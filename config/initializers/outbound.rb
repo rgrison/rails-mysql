12caeed8190814aa8d5fde93d16ed2e52e2fe2ed
Philip Källberg <philip@cloud66.com>
2015-06-04 16:10:01 +0100

    Update outbound.rb

--- config/initializers/outbound.rb
+++ config/initializers/outbound.rb
@@ -1,4 +1,4 @@
 if !#{ENV['CONTAINER_NOTIFY_URL']}.is_empty?
 	puts %x( printenv \n )
-	%x( curl --data "{"ready":true}" #{ENV['CONTAINER_NOTIFY_URL']} )
+	%x( curl --data "ready=true" #{ENV['CONTAINER_NOTIFY_URL']} )
 end
