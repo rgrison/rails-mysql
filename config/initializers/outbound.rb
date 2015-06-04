77412146cc0bce9d24e9292948e637fe4165a0a5
Philip Kallberg <philip@cloud66.com>
2015-06-04 15:43:53 +0100

    update

--- config/initializers/outbound.rb
+++ config/initializers/outbound.rb
@@ -1,2 +1,4 @@
-puts %x( printenv \n )
-%x( curl --data "{"ready":true}" #{ENV['CONTAINER_NOTIFY_URL']} )
+if !#{ENV['CONTAINER_NOTIFY_URL']}.is_empty?
+	puts %x( printenv \n )
+	%x( curl --data "{"ready":true}" #{ENV['CONTAINER_NOTIFY_URL']} )
+end
