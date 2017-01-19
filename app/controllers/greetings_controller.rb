ca754f49d6cc4e3aba04b1b9db4297fb0e4cb043
DimitriosLisenko <DimitriosLisenko@users.noreply.github.com>
2017-01-19 13:36:20 +0000

    Update greetings_controller.rb

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -3,7 +3,7 @@ class GreetingsController < ApplicationController
   caches_page :hello
 
   def hello
-  	@message = "I'm a Rails 4.0.0 Application"
+  	@message = "I'm a Rails #{ Rails::VERSION::STRING } Application"
 
   	ball = Ball.create(:dimples => Random.rand(1000))
   	@balls = "Created a golf ball with #{ball.dimples} dimples!"
