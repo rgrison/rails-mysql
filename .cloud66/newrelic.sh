c6816f0a19a5bd1bc7c7496fe379acb2961a20ae
ciavashkh <siavash@cloud66.com>
2017-07-31 15:51:24 +0100

    test

--- .cloud66/newrelic.sh
+++ .cloud66/newrelic.sh
@@ -1,12 +1,44 @@
-production: # Environment
-    first_thing:
-      - source: /.cloud66/newrelic.sh # Hook type
-        destination: /tmp/newrelic.sh
-        execute: true
-        apply_during: build_only
-        sudo: true
-    after_rails: # Hook point
-      - command: cd $STACK_PATH && chown -R nginx:app_writers tmp/ && chmod -R 775 tmp/ 
-        target: rails 
-        sudo: true
-        
+# {{Description: This deploy hook will run the following code snippet to automate the installation of NewRelic, and requires the NEWRELIC_KEY environment variable to be set on a stack.}}
+
+UBUNTU_VERSION=$(lsb_release -r | awk '{print $2}')
+
+
+if [ -z "$NEWRELIC_KEY" ]
+then
+        >&2 echo "Please set your NEWRELIC_KEY environment variable"
+exit 1
+
+case $UBUNTU_VERSION in
+== 16.04 )
+    printf "license_key: %s\n" "$NEWRELIC_KEY" | sudo tee -a /etc/newrelic-infra.yml
+    curl https://download.newrelic.com/infrastructure_agent/gpg/newrelic-infra.gpg | sudo apt-key add -
+    printf "deb [arch=amd64] https://download.newrelic.com/infrastructure_agent/linux/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/newrelic-infra.list
+    sudo apt-get update
+    sudo apt-get install newrelic-infra -y
+    Message="New relic is installed successfully for Ubuntu 16.04"
+    echo $Message
+    ;;
+
+ == 14.04 )         
+    printf "license_key: %s\n" "$NEWRELIC_KEY" | sudo tee -a /etc/newrelic-infra.yml
+    curl https://download.newrelic.com/infrastructure_agent/gpg/newrelic-infra.gpg | sudo apt-key add -
+    printf "deb [arch=amd64] https://download.newrelic.com/infrastructure_agent/linux/apt trusty main" | sudo tee -a /etc/apt/sources.list.d/newrelic-infra.list
+    sudo apt-get update
+    sudo apt-get install newrelic-infra -y
+    Message="New relic is installed successfully for Ubuntu 14.04"
+    echo $Message
+    ;;
+ == 12.04 )
+    printf "license_key: %s\n" "$NEWRELIC_KEY" | sudo tee -a /etc/newrelic-infra.yml
+    curl https://download.newrelic.com/infrastructure_agent/gpg/newrelic-infra.gpg | sudo apt-key add -
+    printf "deb [arch=amd64] https://download.newrelic.com/infrastructure_agent/linux/apt precise main" | sudo tee -a /etc/apt/sources.list.d/newrelic-infra.list
+    sudo apt-get update
+    sudo apt-get install newrelic-infra -y
+    Message="New relic is installed successfully for Ubuntu 12.04"
+    echo $Message
+    ;;
+ * )
+  Message="It seems this version is not supported"
+  echo $Message && exit 1
+  ;;
+esac
